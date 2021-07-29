d8038e17e4d6a16de4715e5073ad5232105ab42d
Maryna Zahorskaya <zagorskayam@mail.ru>
2021-07-30 00:47:09 +0300

    Add environment information but throw Exception

--- src/test/java/tests/tutbyemail/BaseStep.java
+++ src/test/java/tests/tutbyemail/BaseStep.java
@@ -1,10 +1,13 @@
 package tests.tutbyemail;
 
+import com.google.common.collect.ImmutableMap;
 import driver.Config;
 import driver.DriverSingleton;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
+
+import static com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter;
 
 public class BaseStep {
     static WebDriver driver;
@@ -12,6 +15,15 @@ public class BaseStep {
     @BeforeAll
     public static void initDriver(){
         driver = DriverSingleton.getInstance().getDriver(Config.CHROME);
+    }
+    @BeforeAll
+    public static void setAllureEnvironment() {
+        allureEnvironmentWriter(
+                ImmutableMap.<String, String>builder()
+                        .put("Browser", "Chrome")
+                        .put("Browser.Version", "70.0.3538.77")
+                        .build(), System.getProperty("user.dir") + "/target/allure-results"
+                        );
     }
 
     @AfterAll
